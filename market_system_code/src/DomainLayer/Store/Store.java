249fc3dfcde1e94916f5f9c6a900c401700931e0
shachar <shachco@post.bgu.ac.il>
2024-05-20 12:57:48 +0300

    new functionality for adding product to store

--- market_system_code/src/DomainLayer/Store/Store.java
+++ market_system_code/src/DomainLayer/Store/Store.java
@@ -1,7 +1,11 @@
 package DomainLayer.Store;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Store {
     private int store_ID;
+    private List<Product> storeProducts = new LinkedList<Product>();
 
     Store(int store_ID)
     {
@@ -11,5 +15,9 @@ public class Store {
     public int getStoreID()
     {
         return store_ID;
+    }
+
+    public void addProduct(String productName, int price, int quantity){
+        storeProducts.add(new Product(productName, price, quantity));
     }
 }
