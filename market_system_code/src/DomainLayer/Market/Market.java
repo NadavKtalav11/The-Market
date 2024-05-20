318e682dc5117622ec40c124bd8d862e81e3ed53
Nitzan Horovitz <horovitn@post.bgu.ac.il>
2024-05-20 20:01:33 +0300

    Add user logged in validation before closing a store

--- market_system_code/src/DomainLayer/Market/Market.java
+++ market_system_code/src/DomainLayer/Market/Market.java
@@ -4,6 +4,8 @@ import DomainLayer.Role.RoleFacade;
 import DomainLayer.Store.StoreFacade;
 import DomainLayer.User.UserFacade;
 
+import java.util.List;
+
 public class Market {
     private StoreFacade storeFacade;
     private UserFacade userFacade;
@@ -28,23 +30,24 @@ public class Market {
 
     }
     
-    public void closeStore(int member_ID, int store_ID) throws Exception 
+    public void closeStore(int user_ID, int store_ID) throws Exception
     {
-        if(roleFacade.verifyStoreOwner(store_ID, member_ID) && roleFacade.verifyStoreOwnerIsFounder(store_ID, member_ID))
-        {
-            if(storeFacade.verifyStoreExist(store_ID)) {
-                storeFacade.closeStore(store_ID);
-                List<Integer> storeRoles = roleFacade.getAllStoreRoles(store_ID);
-                //todo: add function which send notification to all store roles (notification component).
-                //todo: update use-case parameters
-            }
-            else {
-                throw new Exception("Store does not exist");
+        if (userFacade.isUserLoggedIn(user_ID)) {
+            int member_ID = this.userFacade.getUsernameByUserID(user_ID);
+            if (roleFacade.verifyStoreOwner(store_ID, member_ID) && roleFacade.verifyStoreOwnerIsFounder(store_ID, member_ID)) {
+                if (storeFacade.verifyStoreExist(store_ID)) {
+                    storeFacade.closeStore(store_ID);
+                    List<Integer> storeRoles = roleFacade.getAllStoreRoles(store_ID);
+                    //todo: add function which send notification to all store roles (notification component).
+                    //todo: update use-case parameters
+                } else {
+                    throw new Exception("Store does not exist");
+                }
+            } else {
+                throw new Exception("Only store founder can close a store");
             }
         }
-        else {
-            throw new Exception("Only store founder can close a store");
-        }
+        else throw new IllegalArgumentException("User is not logged in, so he cannot close a store")
       
     }
 
