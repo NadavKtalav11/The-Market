9346d781be3ed54cbe3eb5df9b4892fcc7389a6d
Nitzan Horovitz <horovitn@post.bgu.ac.il>
2024-05-20 20:54:48 +0300

    added some changes to store closing branch

--- market_system_code/src/DomainLayer/Role/RoleFacade.java
+++ market_system_code/src/DomainLayer/Role/RoleFacade.java
@@ -21,14 +21,6 @@ public class RoleFacade {
         return roleFacadeInstance;
     }
 
-    public boolean verifyStoreOwner(int storeID, String username){
-        for(int i=0 ; i<storeOwnersList.size(); i++){
-            if(storeOwnersList.get(i).getStore_ID() == storeID &&
-                        storeOwnersList.get(i).getUsername() == username){
-                return true;
-            }
-        }
-    }
     public StoreOwner getStoreOwner(int storeID, int memberID)
     {
         for(int i=0 ; i<storeOwnersList.size(); i++){
