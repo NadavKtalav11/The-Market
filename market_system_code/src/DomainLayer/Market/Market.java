5625f2f232f3e070eed27070fc2a01c000aa245d
Nitzan Horovitz <horovitn@post.bgu.ac.il>
2024-05-20 17:33:57 +0300

    Added classes and functions relevant to store closing

--- market_system_code/src/DomainLayer/Market/Market.java
+++ market_system_code/src/DomainLayer/Market/Market.java
@@ -1,4 +1,35 @@
-package com.company.DomainLayer.Market;
+package DomainLayer.Market;
+
+import DomainLayer.Role.RoleFacade;
+import DomainLayer.Store.StoreFacade;
+
+import java.util.List;
 
 public class Market {
+    private StoreFacade storeFacade;
+    private RoleFacade roleFacade;
+
+    Market(){
+        this.roleFacade = RoleFacade.getInstance();
+        this.storeFacade = StoreFacade.getInstance();
+    }
+
+    public void closeStore(int member_ID, int store_ID) throws Exception {
+        if(roleFacade.verifyStoreOwner(store_ID, member_ID) && roleFacade.verifyStoreOwnerIsFounder(store_ID, member_ID))
+        {
+            if(storeFacade.verifyStoreExist(store_ID)) {
+                storeFacade.closeStore(store_ID);
+                List<Integer> storeRoles = roleFacade.getAllStoreRoles(store_ID);
+                //todo: add function which send notification to all store roles (notification component).
+                //todo: update use-case parameters
+            }
+            else {
+                throw new Exception("Store does not exist");
+            }
+        }
+        else {
+            throw new Exception("Only store founder can close a store");
+        }
+    }
+
 }
--- market_system_code/src/DomainLayer/Store/Store.java
+++ market_system_code/src/DomainLayer/Store/Store.java
@@ -6,10 +6,12 @@ import java.util.List;
 public class Store {
     private int store_ID;
     private List<Product> storeProducts = new LinkedList<Product>();
+    private boolean isOpened;
 
     Store(int store_ID)
     {
         this.store_ID = store_ID;
+        this.isOpened = true;
     }
 
     public int getStoreID()
@@ -19,5 +21,10 @@ public class Store {
 
     public void addProduct(String productName, int price, int quantity){
         storeProducts.add(new Product(productName, price, quantity));
+    }
+
+    public void closeStore()
+    {
+        this.isOpened = false;
     }
 }
