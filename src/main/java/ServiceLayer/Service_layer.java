edbe20f3110b67e378cb7d84f2f46b1dc1197fc5
Nitzan Horovitz <horovitn@post.bgu.ac.il>
2024-05-22 22:50:29 +0300

    Added service layer function - branch 8

--- src/main/java/ServiceLayer/Service_layer.java
+++ src/main/java/ServiceLayer/Service_layer.java
@@ -208,4 +208,18 @@ public class Service_layer {
         return allAvailableStores;
     }
 
+    public List<String> getInformationAboutProductInStore(int user_ID, int store_ID)
+    {
+        logger.info("Starting reviewing information about stores in the market.");
+
+        List<String> storeProducts = null;
+        try {
+            storeProducts = market.getInformationAboutProductInStore(user_ID, store_ID);
+        } catch (Exception e) {
+            logger.error("Error occurred during reviewing information about stores in the market: {}", e.getMessage(), e);
+        }
+
+        return storeProducts;
+    }
+
 }
