877c18be5331f039adbf3d45e94db234580fd10d
NadavKtalav11 <ktnadav@gmail.com>
2024-05-26 17:37:09 +0300

    user login logout sense

--- src/main/java/DomainLayer/User/State.java
+++ src/main/java/DomainLayer/User/State.java
@@ -5,10 +5,10 @@ import java.util.Map;
 public interface State {
 
 
-    void Logout(User user);
-    void exitMarketSystem(User user);
+    void Logout();
+    void exitMarketSystem();
     //void Register(User user, String username, String password, String birthday, String address) throws Exception;
-    void Login(String username, String password, Member loginMember) throws Exception;
+    void Login() throws Exception;
     boolean isMember();
     void addReceipt(Map<Integer, Integer> receiptIdAndStoreId);
 }
