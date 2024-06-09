a009942f6136ab3bd63d0804b457f082dd60b9ca
Nitzan Horovitz <horovitn@post.bgu.ac.il>
2024-06-09 21:16:53 +0300

    Added some fix in purchase policy

--- src/main/java/DomainLayer/Store/RulesRepository.java
+++ src/main/java/DomainLayer/Store/RulesRepository.java
@@ -6,11 +6,20 @@ import java.time.LocalDate;
 import java.time.Period;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Map;
 import java.util.function.BiPredicate;
 
 public enum RulesRepository implements TestRule<UserDTO, String>{
 
-    ALCOHOL_RESTRICTION_BELOW_AGE_18((userDTO,productsList) -> {
+    ALCOHOL_RESTRICTION_BELOW_AGE_18((userDTO,productsMap) -> {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/M/yy");
+        LocalDate birthdate = LocalDate.parse(userDTO.getBirthday(), formatter);
+        LocalDate today = LocalDate.now();
+        int age = Period.between(birthdate, today).getYears();
+        return age > 18 ;//|| (age < 18 && !productsList.contains());
+    }),
+
+    BASKET_CONTAINS_LESS_THAN_5KG_TOMATOS((userDTO,productsMap) -> {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/M/yy");
         LocalDate birthdate = LocalDate.parse(userDTO.getBirthday(), formatter);
         LocalDate today = LocalDate.now();
@@ -20,14 +29,14 @@ public enum RulesRepository implements TestRule<UserDTO, String>{
 
     //NAME_STARTS_WITH_B(UserDTO -> UserDTO.getName().startsWith("B"));
 
-    private final BiPredicate<UserDTO, List<String>> predicate;
+    private final BiPredicate<UserDTO, Map<String, List<Integer>>> predicate;
 
-    RulesRepository(BiPredicate<UserDTO, List<String>> predicate) {
+    RulesRepository(BiPredicate<UserDTO, Map<String, List<Integer>>> predicate) {
         this.predicate = predicate;
     }
 
     @Override
-    public BiPredicate<UserDTO, List<String>> getPredicate() {
+    public BiPredicate<UserDTO, Map<String, List<Integer>>> getPredicate() {
         return predicate;
     }
 
