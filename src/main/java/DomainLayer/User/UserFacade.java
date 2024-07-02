7d0f52979908cea0a191c91723bff1921e3a4d0c
NadavKtalav11 <ktnadav@gmail.com>
2024-07-02 18:26:55 +0300

    finish notifications

--- src/main/java/DomainLayer/User/UserFacade.java
+++ src/main/java/DomainLayer/User/UserFacade.java
@@ -195,6 +195,7 @@ public class UserFacade {
 
             Member newMember = new Member(userID, memberId,user.getUserName(), user.getAddress(), user.getName(), password, user.getBirthday(), user.getCountry(), user.getCity());
             members.add(memberId, newMember);
+            getUserByID(userID).addInfo(user);
             //todo pass the user to login page.
             return memberId;
         }
