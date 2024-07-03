15baaf2a53bc32db230ab68630071cb6922ac5f7
assayagt <assayagt@post.bgu.ac.il>
2024-07-03 10:44:57 +0300

    Changed member repository

--- src/main/java/DomainLayer/Repositories/MemberRepository.java
+++ src/main/java/DomainLayer/Repositories/MemberRepository.java
@@ -1,16 +1,16 @@
 package DomainLayer.Repositories;
 
 import DomainLayer.User.Member;
+import DomainLayer.User.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.NoRepositoryBean;
 
 import java.util.List;
 
-public interface MemberRepository {
-    public Member get(String id);
-    public void add(String memberId, Member to_add);
-    public void remove(String to_remove);
-    public List<Member> getAll();
-    public boolean contain(String memberId);
-    public void clear();
-    public Member getByUserName(String member);
+@NoRepositoryBean
+public interface MemberRepository extends JpaRepository<Member,String> {
 
+    @Query("SELECT m FROM Member m WHERE m.username = ?1")
+    Member getByUserName(String member);
 }
