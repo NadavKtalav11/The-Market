c90f89ae4ef30aed51cf8e336fc0bc2e9bc3c3da
Nitzan Horovitz <horovitn@post.bgu.ac.il>
2024-07-03 10:38:38 +0300

    Added StoreManager repositories and db

--- src/main/java/DomainLayer/Role/RoleFacade.java
+++ src/main/java/DomainLayer/Role/RoleFacade.java
@@ -115,7 +115,7 @@ public class RoleFacade {
     }
 
     private void addNewStoreManagerToTheMarket(StoreManager storeManager) {
-        storeManagerRepository.add(storeManager);
+        storeManagerRepository.save(storeManager);
     }
 
     private void addNewStoreOwnerToTheMarket(StoreOwner storeOwner) {
@@ -124,7 +124,7 @@ public class RoleFacade {
 
     public void addNewStoreManagerToTheMarketForTests(StoreManager storeManager) {
         if(!verifyStoreManager(storeManager.getStore_ID(), storeManager.getMember_ID()))
-            storeManagerRepository.add(storeManager);
+            storeManagerRepository.save(storeManager);
     }
 
     public void addNewStoreOwnerToTheMarketForTests(StoreOwner storeOwner) {
@@ -153,7 +153,7 @@ public class RoleFacade {
     public Map<String, List<Integer>> getStoreManagersAuthorizations(String storeID) {
         Map<String, List<Integer>> managersAuthorizations = new HashMap<>();
         for (String memberId : storeManagerRepository.getAllMemberId()) {
-            for (StoreManager currStoreManager : storeManagerRepository.getAllMemberIdOwners(memberId)) {
+            for (StoreManager currStoreManager : storeManagerRepository.getAllMemberIdManagers(memberId)) {
                 if (currStoreManager.getStore_ID().equals(storeID)) {
                     managersAuthorizations.put(memberId, currStoreManager.getAuthorizations());
                 }
@@ -165,7 +165,7 @@ public class RoleFacade {
     public List<String> getAllStoreManagers(String storeID) {
         List<String> storeManagers = new ArrayList<>();
         for (String memberId : storeManagerRepository.getAllMemberId()) {
-            for (StoreManager currStoreManager : storeManagerRepository.getAllMemberIdOwners(memberId)) {
+            for (StoreManager currStoreManager : storeManagerRepository.getAllMemberIdManagers(memberId)) {
                 if (currStoreManager.getStore_ID().equals(storeID)) {
                     storeManagers.add(currStoreManager.getMember_ID());
                 }
@@ -245,7 +245,7 @@ public class RoleFacade {
     }
 
     public void fireStoreManager(String memberIdToFire, String storeID){
-        storeManagerRepository.remove(storeManagerRepository.get(storeID, memberIdToFire));
+        storeManagerRepository.delete(storeManagerRepository.get(storeID, memberIdToFire));
     }
 }
 
